80d2b429597a9e474f8ed2f2c8b93e0a48ec9e83
Anna <ann2005ann55@gmail.com>
2025-04-13 20:45:37 +0500

    demover

--- src/main/java/com/example/demo/controller/PostsViewController.java
+++ src/main/java/com/example/demo/controller/PostsViewController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @Controller
 public class PostsViewController {
@@ -11,5 +13,11 @@ public class PostsViewController {
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String list() {
         return "Здесь будет главная страница";
+    }
+
+    @ResponseBody
+    @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
+    public String single(@PathVariable("id") Long id) {
+        return "Здесь будет страница поста №" + id;
     }
 }
